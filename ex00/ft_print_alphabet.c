1890d40340f95e41335848be91bb1c95190c0d48
Bongani Christopher Simelane <bsimelan@c5r1s9.wethinkcode.co.za>
2020-06-22 11:12:21 +0200

    Doen

--- ex00/ft_print_alphabet.c
+++ ex00/ft_print_alphabet.c
@@ -1,11 +1,5 @@
 #include <unistd.h>
 
-int ft_putchar (char c)
-{
-	write (1,&c,1);
-	return (0);
-}
-
 void ft_print_alphabet (void)
 {
 	char letter = 'a';
@@ -13,10 +7,4 @@ void ft_print_alphabet (void)
 		ft_putchar (letter);
 		letter = letter + 1;
 }
-}
 
-int main ()
-{
-	ft_print_alphabet();
-	return (0);
-}
