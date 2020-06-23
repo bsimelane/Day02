64a3c28bd1604aa90689e474d5851971e6fe6e74
Bongani Christopher Simelane <bsimelan@c5r1s9.wethinkcode.co.za>
2020-06-23 08:39:19 +0200

    ex01

--- ex01/ft_print_reverse_alphabet.c
+++ ex01/ft_print_reverse_alphabet.c
@@ -1,9 +1,21 @@
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsimelan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/23 08:37:10 by bsimelan          #+#    #+#             */
+/*   Updated: 2020/06/23 08:38:04 by bsimelan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 
 void ft_print_reverse_alphabet(void)
 {
 	char letter = 'z';
-	while (letter >= 'a') {
+	while (letter >= 'a') 
+	{
 	ft_putchar(letter);
 	letter = letter-1;
 	}
